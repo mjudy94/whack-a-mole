003f30fb64d45aa5bd068acd41cd4f9279b49974
Dan <saganod1@tcnj.edu>
2014-10-16 15:43:25 -0400

    More files

---
+++ ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  WAMProto
+//
+
+//  Copyright (c) 2014 Dan Saganome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
